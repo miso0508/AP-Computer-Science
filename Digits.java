09cc734e71be31d97575a5db6e9cac9aaa0a53b6
6Shane9 <34317175+6Shane9@users.noreply.github.com>
2018-08-16 07:49:38 -0700

    Update Digits.java

--- Digits.java
+++ Digits.java
@@ -1,27 +1,43 @@
- public static void main(String[] args) {
+    public static void main(String[] args) {
         int ones = OnesDigit(1234);
         int tens = TensOnesDigit(1234);
+        int tensonly = TensDigit(1234);
+        int hundredth = HundredthDigit(1234);
+        int thousandth = ThousandthDigit(1234);
         System.out.println(ones);
         System.out.println(tens);
+        System.out.println(tensonly);
+        System.out.println(hundredth);
+        System.out.println(thousandth);
     }
 
     static int OnesDigit(int a)
     {
-        boolean local = a%10 < 10 && a%10 > 0;
-        while(!local)
-        {
-            a /= 10;
-        }
-        return a%10;
+        return a % 10;
     }
 
     static int TensOnesDigit(int a)
     {
-        int q = a;
-        boolean InTens = q < 100 && q > 9;
-        for(int ten = 10; !InTens; ten = ten * 10)
-        {
-            q = a % ten;
-        }
-        return q;
+        return a % 100;
+    }
+
+    static int TensDigit(int a)
+    {
+        a = a % 100;
+        a = a / 10;
+        return a;
+    }
+
+    static int HundredthDigit(int a)
+    {
+        a = a % 1000;
+        a = a / 100;
+        return a;
+    }
+
+    static int ThousandthDigit(int a)
+    {
+        a = a % 10000;
+        a = a / 1000;
+        return a;
     }
