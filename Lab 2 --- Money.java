9c1266f9974d722db7918466177409a5afc52a4c
6Shane9 <34317175+6Shane9@users.noreply.github.com>
2018-08-21 22:22:03 -0700

    Update Lab 2 --- Money.java

--- Lab 2 --- Money.java
+++ Lab 2 --- Money.java
@@ -19,24 +19,65 @@ class main
     return money;
   }
   
+  public static int[] Convert(int[] m)
+  {
+    if (m[1] > 99)
+    {
+      m[0] += (m[1]/100);
+      m[1] %= 100;
+    }
+    else
+      return m;
+  }
+  
   public static int dollars (int[] money)
   {
+    Convert(money);
     return money[0];
   }
   
   public static int cents (int[] money)
   {
+    Convert(money);
     return money[1];
   }
   
   public static String PrettyPrint (int[] money)
   {
+    Convert(money);
     return ("$" + money[0] + "." + money[1]);
   }
   
   public static String MoneytoText (int[] money)
   {
     //WIP
+  }
+  
+  public static boolean IsGreaterThan (int[] m1, int[] m2)
+  {
+    Convert(m1);
+    Convert(m2);
+    if (m1[0] > m2[0])
+      return true
+    if (m1[0] < m2[0])
+      return false;
+    if (m1[0] == m2[0])
+    {
+      if (m1[1] > m1[2])
+        return true;
+      else
+        return false;
+    }
+  }
+  
+  public static boolean IsEqual (int[] m1, int[] m2)
+  {
+    Convert(m1);
+    Convert(m2);
+    if (m1[0] + m1[1] == m2[0] + m2[1])
+      return true;
+    else
+      return false;
   }
   
 }
