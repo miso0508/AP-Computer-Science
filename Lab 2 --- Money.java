304f598c75d4cc85376f8a236400a1ab6de11ec2
6Shane9 <34317175+6Shane9@users.noreply.github.com>
2018-08-22 13:55:37 -0700

    Update Lab 2 --- Money.java

--- Lab 2 --- Money.java
+++ Lab 2 --- Money.java
@@ -80,4 +80,32 @@ class main
       return false;
   }
   
+  public static void Adder (int[] m1, int[] m2)
+  {
+    m1[0] += m2[0];
+    m1[1] += m2[2];
+    Convert(m1);
+  }
+  
+  public static int[] Add (int[] m1, int[] m2)
+  {
+    int[] sum = new int[2];
+    sum[0] = m1[0] + m2[0];
+    sum[1] = m1[1] + m2[1];
+    Convert(sum);
+  }
+  
+  public static int[] Paywith20 (int[] owe)
+  {
+    int[] change = new int[2];
+    owe[1] = (owe[0] * 100) + owe[1];
+    owe[0] = 0;
+    change[1] = (20 * 100) - owe[1];
+    Convert(change);
+  }
+  
 }
+
+
+
+
