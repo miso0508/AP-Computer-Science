9bbbbc1bebed689a4f5fc229a32abb9e67e7bb87
6Shane9 <34317175+6Shane9@users.noreply.github.com>
2018-08-21 21:52:07 -0700

    Update Lab 2 --- Money.java

--- Lab 2 --- Money.java
+++ Lab 2 --- Money.java
@@ -6,4 +6,37 @@ class main
     
   }
   
+  public static int[] CreateMoney (int dollars, int cents)
+  {
+    int[] money = new int[2];
+    if (cents > 99)
+    {
+      dollars += (cents / 100);
+      cents %= 100;
+    }
+    money[0] = dollars;
+    money[1] = cents;
+    return money;
+  }
+  
+  public static int dollars (int[] money)
+  {
+    return money[0];
+  }
+  
+  public static int cents (int[] money)
+  {
+    return money[1];
+  }
+  
+  public static String PrettyPrint (int[] money)
+  {
+    return ("$" + money[0] + "." + money[1]);
+  }
+  
+  public static String MoneytoText (int[] money)
+  {
+    //WIP
+  }
+  
 }
