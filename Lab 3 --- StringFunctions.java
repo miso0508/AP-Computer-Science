f8c0cd1c61f65b17f0c2adbc37d23db461fc5f06
6Shane9 <34317175+6Shane9@users.noreply.github.com>
2018-08-27 08:32:18 -0700

    Update Lab 3 --- StringFunctions.java

--- Lab 3 --- StringFunctions.java
+++ Lab 3 --- StringFunctions.java
@@ -6,4 +6,16 @@ public class Main
     {
       
     } 
+    
+    public static int SpaceCount(String s)
+    {
+        int spacecount = 0;
+        for(int x = 0; x < s.length(); x++) {
+            if(s.subcount(x,x+1) == " ")
+                spacecount++;
+        }
+        
+        
+        return spacecount;
+    }
 }
