1a3657402800b0acec01794f1972232b90efa146
6Shane9 <34317175+6Shane9@users.noreply.github.com>
2018-08-27 08:44:16 -0700

    Update Lab 3 --- StringFunctions.java

--- Lab 3 --- StringFunctions.java
+++ Lab 3 --- StringFunctions.java
@@ -11,11 +11,22 @@ public class Main
     {
         int spacecount = 0;
         for(int x = 0; x < s.length(); x++) {
-            if(s.subcount(x,x+1) == " ")
+            if(s.substring(x,x+1) == " ")
                 spacecount++;
-        }
-        
-        
+        }      
         return spacecount;
     }
+    
+    public static int VowelCount(String s)
+    {
+        int vowelcount = 0;
+        s = s.toLowerCase();
+        for(int x = 0; x < s.length(); x++) {
+            if(s.substring(x,x+1) == "a" || s.substring(x,x+1) == "e" || s.substring(x,x+1) == "i" || s.substring(x,x+1) == "o" || s.substring(x,x+1) == "u")
+                vowelcount++;
+        }
+        return vowelcount;
+    }
+    
+    public static int LetterCount(String s)
 }
