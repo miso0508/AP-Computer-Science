ef9c4445cada30dd8a7927b777472bd04b0cfd2c
6Shane9 <34317175+6Shane9@users.noreply.github.com>
2018-08-28 09:17:45 -0700

    Update Lab 3 --- StringFunctions.java

--- Lab 3 --- StringFunctions.java
+++ Lab 3 --- StringFunctions.java
@@ -2,19 +2,20 @@ package com.company;
 
 public class Main
 {
-    public static void main (String[] args)
+   public static void main (String[] args)
     {
-        System.out.println(SpaceCount("He llo")); //Ans: 1
-        System.out.println(VowelCount("Hello")); //Ans: 2
-        System.out.println(LetterCount("Helllo","l")); //Ans: 3
-    } 
-    
+	    System.out.println(SpaceCount("He llo")); //Ans: 1
+	    System.out.println(VowelCount("Hello")); //Ans: 2
+	    System.out.println(LetterCount("Helllo","l")); //Ans: 3
+    }
+	
     static int SpaceCount(String s)
     {
         int spacecount = 0;
+        s = s.toLowerCase();
         for(int x = 0; x < s.length(); x++) {
-            if(s.substring(x,x+1) == " ")
-                spacecount++;
+            if((s.substring(x,x+1)).compareTo(" ") == 0)
+                spacecount += 1;
         }
         return spacecount;
     }
@@ -24,8 +25,8 @@ public class Main
         int vowelcount = 0;
         s = s.toLowerCase();
         for(int x = 0; x < s.length(); x++) {
-            if(s.substring(x,x+1) == "a" || s.substring(x,x+1) == "e" || s.substring(x,x+1) == "i" || s.substring(x,x+1) == "o" || s.substring(x,x+1) == "u")
-                vowelcount++;
+            if((s.substring(x,x+1)).compareTo("a") == 0 || (s.substring(x,x+1)).compareTo("e") == 0 || (s.substring(x,x+1)).compareTo("i") == 0 || (s.substring(x,x+1)).compareTo("o") == 0 || (s.substring(x,x+1)).compareTo("u") == 0)
+                vowelcount += 1;
         }
         return vowelcount;
     }
@@ -39,10 +40,10 @@ public class Main
             letters[x] = s.substring(x,x+1);
         }
         for(String element: letters) {
-            if(element == letter)
-                lettercount++;
+            if(element.compareTo(letter) == 0)
+                lettercount += 1;
         }
-
         return lettercount;
+
     }
 }
