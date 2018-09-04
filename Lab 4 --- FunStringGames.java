890409d0ec5d894045f7b6712cc5d69c98d3d026
6Shane9 <34317175+6Shane9@users.noreply.github.com>
2018-09-04 09:16:23 -0700

    Update Lab 4 --- FunStringGames.java

--- Lab 4 --- FunStringGames.java
+++ Lab 4 --- FunStringGames.java
@@ -4,11 +4,39 @@ public class Main
 {
   public static void main (String[] args)
   {
-     
+     String one = "XOXOXOXOXO";
+     System.out.println(stage1(one)); // -5 points
   }
   
   public static int stage1(String s)
   {
+    int points = 0;
+    for(int x = 0; x < s.length(); s++) {
+      if(s.substring(x,x+1) == s.substring(x+1,x+2))
+        points += 0;
+      else {
+        if(s.substring(x,x+1) == "O" && s.substring(x+1,x+2) == "X")
+          points += 1;
+        if(s.substring(x,x+1) == "X" && s.substring(x+1,x+2) == "O")
+          points -= 1;
+      }
+      return points;
+    }
+  }
+  
+  public static int stage2(String s)
+  {
+    int ocount = 0;
+    int xcount = 0;
+    int points = 0;
+    for(int x = 0; x < s.length(); x++) {
+      while(s.substring(x,x+1) == "X") {
+        xcount += 1;
+        x += 1;
+      }
+      points += xcount - 1;
+      
+    }
     
   }
   
