cebb150d8ce67c1514c390fe178a25d723bbd244
6Shane9 <34317175+6Shane9@users.noreply.github.com>
2018-09-11 09:20:44 -0700

    Update Lab 5 --- ArrayFunctions.java

--- Lab 5 --- ArrayFunctions.java
+++ Lab 5 --- ArrayFunctions.java
@@ -1,1 +1,21 @@
+package com.company;
 
+public class Main
+{
+    public static void main (String[] args)
+    {
+      
+    }
+  
+    public static int min(int a, int b)
+    {
+      if(a < b)
+        return a;
+      if(a > b)
+        return b;
+      if(a == b)
+        return a;
+    }
+    
+    
+}
