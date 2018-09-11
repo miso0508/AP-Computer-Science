0f805fb17713aa82c3717a7b2ff8df9432e2e7a5
6Shane9 <34317175+6Shane9@users.noreply.github.com>
2018-09-11 09:23:30 -0700

    Update Lab 5 --- ArrayFunctions.java

--- Lab 5 --- ArrayFunctions.java
+++ Lab 5 --- ArrayFunctions.java
@@ -17,5 +17,14 @@ public class Main
         return a;
     }
     
+    public static int min(int[] a)
+    {
+        int small = a[0];
+        for(int x = 0; x < a.length(); x++) {
+           if(a[x] < small)
+               small = a[x];
+        }
+        return small;
+    }
     
 }
