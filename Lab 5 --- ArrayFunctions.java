e8357a0b26c5471c0f43e103709c0f703994b65f
6Shane9 <34317175+6Shane9@users.noreply.github.com>
2018-09-12 09:47:04 -0700

    Update Lab 5 --- ArrayFunctions.java

--- Lab 5 --- ArrayFunctions.java
+++ Lab 5 --- ArrayFunctions.java
@@ -4,7 +4,45 @@ public class Main
 {
     public static void main (String[] args)
     {
-      
+        //makeSorted()
+        System.out.println("min(3,4)\t3\t" + min(3,4));    
+        System.out.println("min(4,3)\t3\t" + min(3,4));    
+        int[] a = {1,5,3,7,2,65,8};    
+        int[] b = {9,6,34,23,8,6,5,3,2};    
+        System.out.println("min(a)\t\t1\t" + min(a));    
+        System.out.println("min(b)\t\t2\t" + min(b));
+        //add()
+        System.out.println("add(a,b)\t{10,11,37,30,10,71,13,3,2}\t" + pp(add(a,b)));
+        //copy()
+        int[] c = copy(a);    
+        c[0] = 5;    
+        System.out.println("copy(a)\t\t1\t"+a[0]); 
+        //resize()
+        System.out.println("resize(a,3)\t{1,5,3}\t" + pp(resize(a,3)));    
+        System.out.println("resize(a,3)\t7\t" + a.length);          
+        System.out.println("resize(a,9)\t{1,5,3,7,2,65,8,0,0}\t" + pp(resize(a,9)));    
+        System.out.println("resize(a,9)\t7\t" + a.length); 
+        //append()
+        int[] d = {1,2,3,4};    
+        int[] e = {5,6,7};    
+        System.out.println("append(d,e)\t{1,2,3,4,5,6,7}\t" + pp(append(d,e)));   
+        System.out.println("append(d,e)\t4\t" + d.length);    
+        System.out.println("append(d,e)\t3\t" + e.length);    
+        System.out.println("append(null,e)\t{5,6,7}\t\t" + pp(append(null,e)));    
+        System.out.println("append(d,null)\t{1,2,3,4}\t" + pp(append(d,null)));
+        //subArray()
+        System.out.println("subArray(a,3,5)\t{7,2}\t" + pp(subArray(a,3,5)));    
+        System.out.println("subArray()\t7\t" + a.length);    
+        System.out.println("subArray(a,4,7)\t{2,65,8}\t" + pp(subArray(a,4,7)));    
+        System.out.println("subArray(a,3,3)\tnull\t" + pp(subArray(a,3,3)));    
+        System.out.println("subArray(a,5,3)\tnull\t" + pp(subArray(a,5,3))); 
+        //makeSorted()
+        makeSorted(a);    
+        System.out.println("makeSorted(a)\t{1,2,3,5,7,8,65}\t" + pp(a)); 
+        //sort()
+        System.out.println("sort(b)\t{2,3,5,6,6,8,9,23,34}\t" + pp(sort(b))); 
+        //merge()
+         System.out.println("merge(a,d)\t{1,1,2,2,3,3,4,5,7,8,65}\t" + pp(merge(a,d)));         
     }
   
     public static int min(int a, int b)
@@ -27,4 +65,45 @@ public class Main
         return small;
     }
     
+    public static int[] add(int[] a, int[] b)
+    {
+        int min = 0, max = 0;
+        boolean isBigger = false;
+        if(a.length() > b.length()) {
+            max = a.length();
+            min = b.length();
+            isBigger = true;
+        }
+        else {
+            max = b.length();
+            min = a.length();
+        }
+        int[] rv = new int[max];
+        for(int x = 0; x < min; x++) {
+            rv[x] = a[x] + b[x];
+        }
+        for(int x = min; x < max; x++) {
+            if(isBigger == true)
+                rv[x] = a[x];
+            else
+                rv[x] = b[x];
+        }
+        return rv;
+    }
+    
+    public static String prettyPrint(int[] a)
+    {
+        String rv = "{ ";
+        for(int x = 0; x < a.length(); x++) {
+            rv += a[x] + ", ";
+        }
+        rv += "}";
+        return rv;
+    }
+    
+    public static int[] copy(int[] a)
+    {
+        
+    }
+     
 }
