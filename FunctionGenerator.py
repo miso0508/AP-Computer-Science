a5393d1dac7fb347ffc172a740cf8f65a6d5ade1
6Shane9 <34317175+6Shane9@users.noreply.github.com>
2018-09-26 10:30:16 -0700

    Update FunctionGenerator.py

--- FunctionGenerator.py
+++ FunctionGenerator.py
@@ -1,1 +1,16 @@
+import math;
+import random;
+def MakeFunction():
+  terms = random.randint(1,5)
+  powcount = random.randint(1,6)
+  ar_op = "+"
+  func_str = ""
+  for(x in range(terms)):
+    if(random.randint(0,1) == 0):
+      ar_op = " - "
+    else:
+      ar_op = " + "
+    func_str += (str(random.randint(1,10)) + 'x^' + str(random.randint(1,powcount - x))
+  print(func_str)
 
+MakeFunction()
