cc67491e44534597ed0c6e2b32a2a21fb8540d9c
6Shane9 <34317175+6Shane9@users.noreply.github.com>
2018-10-01 09:23:17 -0700

    Update Lab 7 --- Stacks and Queues.java

--- Lab 7 --- Stacks and Queues.java
+++ Lab 7 --- Stacks and Queues.java
@@ -1,1 +1,39 @@
+package com.company;
+
+public class Main
+{
+    public static void main (String[] args)
+    {
+      
+    }
+    
+    static int[] createStack()
+    {
+        int[] stack = new int[101];
+        stack[0] = 0;
+        return stack;
+    }
+    
+    static void push(int[] stack, int data)
+    {
+        int[] e = stack;
+        stack = new int[e.length + 1];
+        for(int x = 0; x < stack.length; x++)
+            stack[x] = e[x];
+        stack[e.length] = data;
+    }
+    
+    static int pop(int[] stack)
+    {
+        int[] e = stack;
+        stack = new int[e.length - 1];
+        for(int x = 0; x < stack.length; x++)
+            stack[x] = e[x];
+        return e[stack.length];
+    }
+  
+}
+
+
+
 
