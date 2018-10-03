e2c4600d39da36668eeb9f4358501922b72da2ba
6Shane9 <34317175+6Shane9@users.noreply.github.com>
2018-10-03 09:15:51 -0700

    Update Lab 7 --- Stacks and Queues.java

--- Lab 7 --- Stacks and Queues.java
+++ Lab 7 --- Stacks and Queues.java
@@ -4,34 +4,93 @@ public class Main
 {
     public static void main (String[] args)
     {
-      
+        //TASK 1
+        int[] stack = createStack();
+        for(int x = 9; x >= 3; x--)
+            push(stack, x);
+        pop(stack);
+        pop(stack);
+        pop(stack);
+        //Value of 3rd pop = 5
+        for(int x = 1; x <= 4; x++)
+            push(stack, x);
+        for(int x = 1; x <= 5; x++)
+            pop(stack);
+        //Value of 5th pop = 6
+        System.out.println(prettyPrintStack(stack));
+        
+        //TASK 2
+        int[] restroomList = createStack();
+        /* 
+           It is not a good choice because if you pop the array, last people in line
+           will go first, not the first people in line 
+        */
+        for(int x = 1; x <= 10; x++)
+            push(stack, x);
+        System.out.println(prettyPrintStack(restroomList));
+        pop(stack);
+        System.out.println(prettyPrintStack(restroomList));
+        
+        //TASK 3
+        
     }
     
     static int[] createStack()
     {
-        int[] stack = new int[103];
-        stack[0] = 101;
+        int[] stack = new int[101];
+        stack[0] = 0;
         return stack;
     }
     
     static void push(int[] stack, int data)
     {
-        int[] e = stack;
-        stack = new int[e.length + 1];
-        for(int x = 0; x < e.length; x++)
-            stack[x] = e[x];
-        stack[0] += 1;
-        stack[stack.length - 1] = 0;
-        stack[e.length] = data;
+        if(stack[0] >= 100)
+            System.out.println("Stack Overflow Error");
+        else {
+            stack[stack[0] + 1] = data;
+            stack[0]++;
+        }
     }
     
     static int pop(int[] stack)
     {
-        int[] e = stack;
-        stack = new int[e.length - 1];
-        for(int x = 0; x < stack.length; x++)
-            stack[x] = e[x];
-        return e[stack.length];
+        stack[0]--;
+        return stack[stack[0] + 1]
+    }
+    
+    static String prettyPrintStack(int[] stack)
+    {
+        String s = "[";
+        for(int x = 1; x <= stack[0]; x++)
+            s += stack[x] + ",";
+        s += "]";
+        return s;
+    }
+    
+    static String dumpStack(int[] stack)
+    {
+        String s = "{";
+        for(int x = 0; x < 101; x++)
+            s += stack[x] + ",";
+        s += "}";
+        return s;
+    }
+    
+    static int[] createQueue()
+    {
+        int[] q = new int[101];
+        q[0] = 0;
+        return q;
+    }
+    
+    static void enqueue(int[] queue, int data)
+    {
+        if(queue[0] >= 100)
+            System.out.println("Queue Overflow Error");
+        else {
+            queue[0]++;
+            queue[queue[0]] = data;
+        }
     }
   
 }
