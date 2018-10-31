cb65004a72cb889d76b35d1cd3afdd78ecfb4efb
6Shane9 <34317175+6Shane9@users.noreply.github.com>
2018-10-31 08:42:28 -0700

    Update Lab 11 --- DateClass.java

--- Lab 11 --- DateClass.java
+++ Lab 11 --- DateClass.java
@@ -23,7 +23,15 @@ public class DateClass
     m = mmddyyyy / 1000000;
     d = mmddyyyy / 10000 % 100;
     y = mmddyyyy % 10000;
-    if((m == 2 && d > 28)  || (m == 1 && m > 31) || (m > 2 && m 
+    if((m == 2 && d > 28)  || (m == 1 && m > 31) || (m > 2 && (m-3)%2 == 0 && d > 31) || ((m-4)%2 == 0 && d > 30)) {
+      m = 0;
+      d = 0;
+      y = 0;
+      System.out.println("Invalid Date");
+    }
+    this.month = m;
+    this.day = d;
+    this.year = y;
   }
   public String get()
   {
@@ -34,5 +42,13 @@ public class DateClass
     this.day = day;
     this.month = month;
     this.year = year;
+  }
+  public boolean isLeapYear(DateClass date)
+  {
+    boolean isleapyear = false;
+    if(date.year % 4 == 0) {
+      isleapyear = true;
+      if(date.year % 100 == 0 && date.year
+    }
   }
 }
