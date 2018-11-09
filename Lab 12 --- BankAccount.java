a29219808958f71d29ec03737ec2af057f071ca7
6Shane9 <34317175+6Shane9@users.noreply.github.com>
2018-11-09 08:50:46 -0800

    Update and rename :ab 12 --- BankAccount.java to Lab 12 --- BankAccount.java

---
+++ Lab 12 --- BankAccount.java
@@ -0,0 +1,16 @@
+public class Main
+{
+  public static void main(String[] args)
+  {
+  }
+}
+
+class BankAccount
+{
+  private int numberOfTransactions=0;
+  private int numberOfBankAccounts=0;
+  private String firstName, lastName, middleInitial; // Mid initial no period
+  private Money balance;
+  private String bankName;
+
+}
