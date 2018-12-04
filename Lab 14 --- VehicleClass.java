c7e00956b508e02f3e772b98fad25ff168a5947a
6Shane9 <34317175+6Shane9@users.noreply.github.com>
2018-12-04 08:24:18 -0800

    Update Lab 14 --- VehicleClass.java

--- Lab 14 --- VehicleClass.java
+++ Lab 14 --- VehicleClass.java
@@ -1,1 +1,18 @@
+import java.lang.Math;
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+  }
+  
+  public class Car 
+  {
+    private int seats, passengers;
+    private Point current = new Point();
+    private Point dest = new Point();
+    
+    
+  }
+}
 
