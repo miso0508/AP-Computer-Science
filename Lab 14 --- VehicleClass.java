cc2ea1f3c6ba30693567674f1f936ee04308fb5b
6Shane9 <34317175+6Shane9@users.noreply.github.com>
2018-12-04 08:39:18 -0800

    Update Lab 14 --- VehicleClass.java

--- Lab 14 --- VehicleClass.java
+++ Lab 14 --- VehicleClass.java
@@ -5,13 +5,55 @@ public class Main
   public static void main(String[] args)
   {
   }
-  
-  public class Car 
+  public class Point
   {
-    private int seats, passengers;
-    private Point current = new Point();
-    private Point dest = new Point();
+    private double x,y;
+    public Point()
+    {
+    }
+    public Point(double x, double y)
+    {
+      this.x = x;
+      this.y = y;    
+    }
+    public double getX()
+    {
+      return this.x;
+    }
+    public double getY()
+    {
+      return this.y;
+    }
+  }
     
+  
+  public class Vehicle
+  {
+    public int seats, passengers, speed;
+    public Point current = new Point();
+    public Point dest = new Point();
+    public Vehicle(int s, int p, int sp, Point c, Point d) {
+      seats = s;
+      passengers = p;
+      current = c;
+      dest = d;
+      speed = sp;
+    }
+    public Vehicle() {
+    }
+    public String communicate() {
+      return "Hey there!";
+    }
+    public String toStr() {
+      return "This is a vehicle";
+    }
+  }
+  
+  public class Airplane extends Vehicle
+  {
+    String flight_num;
+    public Airplane() {
+    }
     
   }
 }
