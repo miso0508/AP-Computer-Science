909e22e9f9a55e9e2f25aa25b2b14fb4baca9636
nidarshans <34317175+nidarshans@users.noreply.github.com>
2019-03-02 15:42:03 -0800

    Update Lab 20 - Using Array Lists.java

--- Lab 20 - Using Array Lists.java
+++ Lab 20 - Using Array Lists.java
@@ -1,16 +1,37 @@
 import java.util.ArrayList;
 
-public class Main {
-    ArrayList<String> str = new ArrayList<String>();
-    for(int x = 2; x <= 100; x++) {
-        str.add("" + x);
-    }
-    for(int a = 0; a < str.size(); a++) {
-        for(int b = a + 1; b < str.size(); b++) {
-            if(Integer.ParseInt(str.get(b)) % Integer.ParseInt(str.get(a)) == 0) str.set(b,"NULL");
+class Main {
+    public static void main(String[] args) {
+        ArrayList<String> str = new ArrayList<String>();
+        for(int x = 2; x <= 10000; x++) {
+            str.add("" + x);
+        }
+        for(int a = 0; a < str.size(); a++) {
+            for(int b = a + 1; b < str.size(); b++) {
+                if(Integer.parseInt(str.get(b)) % (a + 2) == 0) {
+                    str.set(b,"1");
+                }
+            }
+        }
+        for(int x = 0; x < str.size(); x++) {
+            if(str.get(x).compareTo("1") == 0) {
+                str.remove(x);
+                x--;
+            }
+        }
+        System.out.println("Index\tPrime Number");
+        for(int x = 0; x < str.size(); x++) {
+            System.out.println(x + "\t" + str.get(x));
         }
     }
-    for(int x = 0; x < str.size(); x++) {
-        if(s.compareTo("NULL") == 1) str.remove(x);
+
+    public static boolean isPrime(int p) {
+        if(p == 1) return false;
+        else {
+            for(int x = 2; x < p; x++) {
+                if(p % x == 0) return false;
+            }
+        }
+        return true;
     }
 }
